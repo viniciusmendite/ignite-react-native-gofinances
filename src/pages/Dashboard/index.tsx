00f5a1f2bc2bb4822825724a6043b829cdda6eb7
Vinicius Mendite <vinicius@sharbe.com.br>
2021-07-07 16:27:29 -0300

    create header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
-import { Container } from './styles'
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+} from './styles'
 
 export function Dashboard() {
   return (
     <Container>
-
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/53957541?v=4' }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Vinícius</UserName>
+            </User>
+          </UserInfo>
+          
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
